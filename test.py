f0a7bf27b7a263f97063fdf0f1aa582b1b1b255e
VikasManohar <vikasmanohar.1995@gmail.com>
2019-02-22 21:24:36 +0530

    Added score board, cropped the car, detect if car hit obstacle

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # import the pygame module, so you can use it
 import pygame
 import time
+import random
 # initialize the pygame module
 pygame.init()
 
@@ -11,7 +12,7 @@ black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 
-car_width = 100
+car_width = 50
 # load and set the logo
 # create a surface on screen that has the size of display_width and display_height
 gameDisplay = pygame.display.set_mode((display_width,display_height))
@@ -21,7 +22,15 @@ pygame.display.set_caption("pyGame Test")
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('car.png')
-carImg = pygame.transform.scale(carImg, (100,100))
+carImg = pygame.transform.scale(carImg, (50,100))
+
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: " + str(count), True, black)
+    gameDisplay.blit(text, (0,0))
+
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
 
 def car(x,y):
     gameDisplay.blit(carImg, (x,y))
@@ -51,7 +60,14 @@ def game_loop():
     y = (display_height * 0.8)
      
     x_change = 0
-     
+    
+    thing_width = 100
+    thing_height = 100
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 7
+    dodged = 0
+    
     # define a variable to control the main loop
     gameExit = False
      
@@ -76,13 +92,27 @@ def game_loop():
         
         x += x_change
         
-        
-        
         gameDisplay.fill(white)
+        
+        
+        things(thing_startx, thing_starty, thing_width, thing_height, black)
+        thing_starty += thing_speed
         car(x,y)
+        things_dodged(dodged)
         
         if x > display_width - car_width or x < 0:
             crash()
+        
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0, display_width)
+            dodged += 1
+#            thing_speed += 1
+#            thing_width += (dodged * 1.2)
+            
+        if y < thing_starty + thing_height:
+            if x > thing_startx and x < thing_startx + thing_width or x + car_width > thing_startx and x + car_width < thing_startx + thing_width:
+                crash()
             
         pygame.display.update()
         clock.tick(60)
